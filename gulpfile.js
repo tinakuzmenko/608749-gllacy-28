534b974f28ae15faca89f0999b5081a8d8dcabf2
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-10-27 16:42:55 +0100

    Добавляет конфиги для сборки, добавляет препроцессор для стилей

---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+"use strict";
+
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var del = require("del");
+
+gulp.task("css", () => {
+  return gulp
+    .src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("images", () => {
+  return gulp
+    .src("source/img/**/*{png,jpg,svg}")
+    .pipe(
+      imagemin([
+        imagemin.optipng({ optimizationLevel: 3 }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.svgo()
+      ])
+    )
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("html", () => {
+  return gulp
+    .src("source/*.html")
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", () => {
+  return del("build");
+});
+
+gulp.task("copy", () => {
+  return gulp
+    .src(
+      [
+        "source/fonts/**/*.{woff,woff2}",
+        "source/img/**",
+        "source/js/**",
+        "source/vendors/**",
+        "source/*.ico",
+      ],
+      {
+        base: "source"
+      }
+    )
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("refresh", (done) => {
+  server.reload();
+  done();
+});
+
+gulp.task("server", () => {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+});
+
+gulp.task("build", gulp.series("clean", "copy", "css", "html"));
+gulp.task("start", gulp.series("build", "server"));
